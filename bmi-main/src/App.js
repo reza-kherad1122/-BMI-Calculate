9923c06195ad2e198c74efb420d78ac54452c64e
Reza Kherad <kherad148@gmail.com>
2025-05-23 13:29:04 +0330

    Add useState

---
+++ bmi-main/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+
+  );
+}

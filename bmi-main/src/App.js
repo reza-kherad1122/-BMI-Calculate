74f754a5593e5ce810dfc8496036264c1bb05f2b
Reza Kherad <kherad148@gmail.com>
2025-05-23 13:36:33 +0330

    Added a header site

--- bmi-main/src/App.js
+++ bmi-main/src/App.js
@@ -9,6 +9,28 @@ function App() {
   const [message, setMessage] = useState("");
 
   return (
-
+    <div className="app">
+    <div className='container'>
+      <h2 className='center'>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (lbs)</label>
+            <input type="text" placeholder='Enter Weight value' value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input type="text" placeholder='Enter height value' value={height} onChange={(event) => setHeight(event.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+        </form>
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+    </div>
+  </div>
   );
 }
